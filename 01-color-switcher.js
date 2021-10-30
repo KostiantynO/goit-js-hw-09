5e7b21c802d9192151667ef061fd5c07a9bb6e65
Kostiantyn Ochenash <61573649+KostiantynO@users.noreply.github.com>
2021-10-31 00:31:31 +0300

    Add files via upload

---
+++ 01-color-switcher.js
@@ -0,0 +1,40 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+console.log(btnStart);
+
+let changing = false;
+
+const switchBgColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStartBtnClick = e => {
+  changing = true;
+  e.currentTarget.setAttribute('disabled', 'true');
+
+  do {
+    setTimeout(switchBgColor, 250);
+  } while (changing === true);
+
+  btnStop.addEventListener('click', onStopBtnClick, {
+    once: true
+  });
+};
+
+const onStopBtnClick = e => {
+  changing = false;
+  btnStart.removeAttribute('disabled');
+
+  btnStart.addEventListener('click', onStartBtnClick, {
+    once: true,
+  });
+};
+
+btnStart.addEventListener('click', onStartBtnClick, {
+  once: true,
+});
